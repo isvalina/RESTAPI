207b0c5f2908627f563260b9385f56bef17487d3
Cvijetin <ivanasvalina15@gmail.com>
2020-11-22 19:55:07 +0100

    first commit

---
+++ src/Svalina/Mjerenje.php
@@ -0,0 +1,23 @@
+<?php
+namespace Svalina;
+
+use stdClass;
+use Faker\Factory;
+
+class Mjerenje
+{
+    public function getMjerenja()
+    {
+        $faker = Factory::create('hr_HR');
+        $mjerenja=[];
+        //dovući podatke iz baze
+        for($i=1;$i<26;$i++){
+            $m = new stdClass();
+            $m->sifra=$i;
+            $m->datum=$faker->iso8601($max = 'now');
+            $m->iznos=$faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = NULL);
+            $mjerenja[]=$m;
+        }
+        return $mjerenja;
+    }
+}
